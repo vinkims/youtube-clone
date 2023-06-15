620f5d36620b50259ef256e6ac874ab96e463ea0
vinkims <vincekigen@gmail.com>
2023-06-15 17:05:26 +0300

    Add RapidAPI

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { Box } from '@mui/material';
 
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
-const App = () => {
-  <BrowserRouter>
+const App = () => (
+  <BrowserRouter basename='/'>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path='/' exact element={<Feed />} />
+        <Route exact path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail /> } />
         <Route path='/channel/:id' element={<ChannelDetail /> } />
         <Route path='/search/:searchTerm' element={<SearchFeed /> } />
       </Routes>
     </Box>
   </BrowserRouter>
-}
+);
 
-export default App
+export default App;
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { demoThumbnailUrl, demoVideoTitle, demoVideoUrl, demoChannelTitle, demoChannelUrl } from '../utils/constants';
+
+const VideoCard = ({ video: { id: {videoId }, snippet } }) => {
+
+  return (
+   <Card>
+    <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+      <CardMedia image={snippet?.thumbnails?.high?.url} />
+    </Link>
+   </Card>
+  )
+}
+
+export default VideoCard
